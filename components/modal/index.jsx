6e05b08ca19224d94219f7209a3c8808860e3a73
Shahidul <ishahin56@gmail.com>
2022-06-08 19:38:14 +0600

    Completed modal responsive style

--- components/modal/index.jsx
+++ components/modal/index.jsx
@@ -4,8 +4,8 @@ import styles from './Modal.module.css';
 
 const Modal = ({children, handleClose}) => {
   return (
-    <div className={`d-flex align-center justify-center ${styles.modal_wrap}`}>
-        <div className={`d-flex align-center justify-center ${styles.modal_container}`}>
+    <div className={`${styles.modal_wrap}`}>
+        <div className={`d-flex align-center ${styles.modal_container}`}>
             <div className={styles.modal_inner}>
                 <button className={`btn d-flex align-center justify-center ${styles.btn_modal_close}`} onClick={handleClose}>
                     <CloseRound className={styles.close_icon} />
